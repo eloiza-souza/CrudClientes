d9ad2aab5e0aa0b3bac529768af7d4b9fe8b332a
Eloiza <eloiza.souza@zup.com.br>
2025-02-14 16:07:42 -0300

    feat: adiciona o método getAllClients na classe ClientController

--- src/main/java/com/crud/CrudClientes/controllers/ClientController.java
+++ src/main/java/com/crud/CrudClientes/controllers/ClientController.java
@@ -1,8 +1,12 @@
 package com.crud.CrudClientes.controllers;
 
+import com.crud.CrudClientes.dtos.ClientDTO;
 import com.crud.CrudClientes.services.ClientService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/clients")
@@ -12,6 +16,11 @@ public class ClientController {
 
     public ClientController(ClientService clientService) {
         this.clientService = clientService;
+    }
+
+    @GetMapping
+    public List<ClientDTO> getAllClients(){
+        return clientService.getAllClients();
     }
 
 
