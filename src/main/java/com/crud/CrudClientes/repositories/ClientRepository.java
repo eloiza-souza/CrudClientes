c077b00afa0cf8f6dbe2b2ff0e7e7168a36d7812
Eloiza <eloiza.souza@zup.com.br>
2025-02-14 15:11:29 -0300

    refactor: muda importação do Client para ClientModel no ClientRepository

--- src/main/java/com/crud/CrudClientes/repositories/ClientRepository.java
+++ src/main/java/com/crud/CrudClientes/repositories/ClientRepository.java
@@ -1,9 +1,9 @@
 package com.crud.CrudClientes.repositories;
 
-import com.crud.CrudClientes.models.Client;
+import com.crud.CrudClientes.models.ClientModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ClientRepository extends JpaRepository<Client, Long> {
+public interface ClientRepository extends JpaRepository<ClientModel, Long> {
 }
