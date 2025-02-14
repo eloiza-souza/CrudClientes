d54bd3bc628aa2139fa9571ac3f67760f5a92f73
Eloiza <eloiza.souza@zup.com.br>
2025-02-14 15:22:33 -0300

    feat: cria ClientController com atributo clientService e construtor

---
+++ src/main/java/com/crud/CrudClientes/controllers/ClientController.java
@@ -0,0 +1,18 @@
+package com.crud.CrudClientes.controllers;
+
+import com.crud.CrudClientes.services.ClientService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/clients")
+public class ClientController {
+
+    private final ClientService clientService;
+
+    public ClientController(ClientService clientService) {
+        this.clientService = clientService;
+    }
+
+
+}
