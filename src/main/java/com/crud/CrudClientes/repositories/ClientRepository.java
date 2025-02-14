c194117251b348d83ea605e90024454cc106aaa1
Eloiza <eloiza.souza@zup.com.br>
2025-02-14 14:59:31 -0300

    refactor: adiciona anotação @Repository na ClientRepository

--- src/main/java/com/crud/CrudClientes/repositories/ClientRepository.java
+++ src/main/java/com/crud/CrudClientes/repositories/ClientRepository.java
@@ -2,6 +2,8 @@ package com.crud.CrudClientes.repositories;
 
 import com.crud.CrudClientes.models.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
 }
