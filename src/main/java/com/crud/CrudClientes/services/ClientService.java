e3d4133cae4e9cc1f9b33534918bb5c4faf6947b
Eloiza <eloiza.souza@zup.com.br>
2025-02-14 15:10:01 -0300

    feat: adiciona método getAllClients na ClientService

--- src/main/java/com/crud/CrudClientes/services/ClientService.java
+++ src/main/java/com/crud/CrudClientes/services/ClientService.java
@@ -1,7 +1,10 @@
 package com.crud.CrudClientes.services;
 
+import com.crud.CrudClientes.models.ClientModel;
 import com.crud.CrudClientes.repositories.ClientRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ClientService {
@@ -9,5 +12,10 @@ public class ClientService {
 
     public ClientService(ClientRepository clientRepository) {
         this.clientRepository = clientRepository;
+    }
+
+    public List<ClientModel> getAllClients(){
+        List<ClientModel> clients = clientRepository.findAll();
+        return clients;
     }
 }
